7b5e17914a640dc95c20fcd50307aaa36c84f930
Kumod Yadav <kumod353@gmail.com>
2024-03-05 21:22:37 +0530

    Initial Commit

---
+++ LOTRConvertor17/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LOTRConvertor17
+//
+//  Created by Kumod Yadav on 15/12/1402 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTRConvertor17/LOTRConvertor17App.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConvertor17App.swift
+//  LOTRConvertor17
+//
+//  Created by Kumod Yadav on 15/12/1402 AP.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConvertor17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
